689cc70ebd08e279ad977cb2cdd93b583d21469c
Istvan <istvan.toth@astutis.com>
2022-01-20 10:58:00 +0000

    dynamic loader bg change test

--- JS/transitionv2.js
+++ JS/transitionv2.js
@@ -1,5 +1,6 @@
 //Loading animation V3
 window.onload = function () {
+  /*Working solution
   const anchors = document.querySelectorAll("a");
   const transition_el = document.querySelector(".transition");
   console.log("loaded");
@@ -12,25 +13,44 @@ window.onload = function () {
     transition_el.classList.add("hidden");
     document.body.classList.remove("ast-no-scroll");
   }, 900);
+  */
 
-  // for (let i = 0; i < anchors.length; i++) {
-  //   const anchor = anchors[i];
+  //Test
+  const anchors = document.querySelectorAll("a");
+  const transition = document.querySelector(".drawing");
+  const transition_el = document.querySelector(".transition");
+  console.log("loaded");
+  document.body.classList.add("ast-no-scroll");
+  transition.style.backgroundColor = "red";
 
-  //   anchor.addEventListener("click", (e) => {
-  //     e.preventDefault();
-  //     let target = e.target.href;
+  setTimeout(() => {
+    transition_el.classList.remove("is-active");
+  }, 500);
+  setTimeout(() => {
+    transition_el.classList.add("hidden");
+    document.body.classList.remove("ast-no-scroll");
+  }, 900);
 
-  //     console.log(transition_el);
-  //     transition_el.classList.remove("hidden");
+  /*
+  for (let i = 0; i < anchors.length; i++) {
+    const anchor = anchors[i];
 
-  //     setTimeout(() => {
-  //       transition_el.classList.add("is-active");
-  //     }, 100);
-  //     console.log(transition_el);
+    anchor.addEventListener("click", (e) => {
+      e.preventDefault();
+      let target = e.target.href;
 
-  //     setTimeout(() => {
-  //       window.location.href = target;
-  //     }, 500);
-  //   });
-  // }
+      console.log(transition_el);
+      transition_el.classList.remove("hidden");
+
+      setTimeout(() => {
+        transition_el.classList.add("is-active");
+      }, 100);
+      console.log(transition_el);
+
+      setTimeout(() => {
+        window.location.href = target;
+      }, 500);
+    });
+  }
+  */
 };
