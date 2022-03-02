d94d25b232b284c5da8c2564ce7cfd99d46b25e4
Istvan <istvan.toth@astutis.com>
2022-03-02 14:46:20 +0000

    new parameters

--- JS/lottie-interactivity.js
+++ JS/lottie-interactivity.js
@@ -3,26 +3,32 @@ class Lottie {
   constructor() {
     this.ids = document.querySelectorAll("[id^='ast-lottie-play--']");
   }
-  scrollSync(id) {
+  scrollSync(
+    id,
+    positionStart = 0,
+    positionEnd = 1.0,
+    frameStart = 0,
+    frameEnd = 300
+  ) {
     LottieInteractivity.create({
       player: `#${id}`,
       mode: "scroll",
       actions: [
         {
-          visibility: [0, 1.0],
+          visibility: [positionStart, positionEnd],
           type: "seek",
-          frames: [0, 300],
+          frames: [frameStart, frameEnd],
         },
       ],
     });
   }
-  visible(id, position = 0.5) {
+  visible(id, positionStart = 0.5, positionEnd = 1.0) {
     LottieInteractivity.create({
       player: `#${id}`,
       mode: "scroll",
       actions: [
         {
-          visibility: [position, 1.0],
+          visibility: [positionStart, positionEnd],
           type: "play",
         },
       ],
@@ -39,15 +45,35 @@ class Lottie {
       ],
     });
   }
+  hover(id) {
+    LottieInteractivity.create({
+      player: `#${id}`,
+      mode: "cursor",
+      actions: [
+        {
+          type: "hover",
+          forceFlag: false,
+        },
+      ],
+    });
+  }
   automate() {
     this.ids.forEach((id) => {
       console.log(id);
       if (id.classList.contains("ast-lottie--scroll")) {
-        this.scrollSync(id.id);
+        this.scrollSync(
+          id.id,
+          id.dataset.visiblestart,
+          id.dataset.visibleend,
+          id.dataset.framestart,
+          id.dataset.frameend
+        );
       } else if (id.classList.contains("ast-lottie--visible")) {
-        this.visible(id.id, id.dataset.visible);
+        this.visible(id.id, id.dataset.visiblestart, id.dataset.visibleend);
       } else if (id.classList.contains("ast-lottie--toggle")) {
         this.toggle(id.id);
+      } else if (id.classList.contains("ast-lottie--hover")) {
+        this.hover(id.id);
       } else {
         this.visible(id.id);
       }
