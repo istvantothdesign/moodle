6664e09218cba592d70d0ee151108bff2dc165fd
Istvan <istvan.toth@astutis.com>
2022-06-24 16:30:50 +0100

    activityhover added

---
+++ Astutis Theme/Alpha/JS/activityhover.js
@@ -0,0 +1,60 @@
+const astPageID = document.getElementById("page-course-view-topics");
+
+if (astPageID.id === "page-course-view-topics") {
+  console.log("yes");
+  const topics = document.querySelectorAll(".section-summary");
+  const activities = document.querySelectorAll(".activity.activity-wrapper");
+  console.log(topics);
+
+  for (let i = 0; i < topics.length; ++i) {
+    topics[i].addEventListener("click", function () {
+      let link = this.childNodes[1].childNodes[1].childNodes[1].href;
+      console.log(link);
+      window.location.href = `${link}`;
+    });
+
+    // This handler will be executed every time the cursor
+    // is moved over a different list item
+    topics[i].addEventListener(
+      "mouseover",
+      function (event) {
+        // highlight the mouseover target
+        //   event.target.style.background = "orange";
+        event.target.style.cursor = "pointer";
+
+        //   // reset the color after a short delay
+        //   setTimeout(function () {
+        //     event.target.style.background = "green";
+        //   }, 500);
+      },
+      false
+    );
+  }
+
+  for (let i = 0; i < activities.length; ++i) {
+    activities[i].addEventListener("click", function () {
+      let link =
+        this.childNodes[1].childNodes[1].childNodes[1].childNodes[1]
+          .childNodes[1].childNodes[3].childNodes[3].childNodes[1].href;
+      console.log(link);
+      window.location.href = `${link}`;
+    });
+
+    // This handler will be executed every time the cursor
+    // is moved over a different list item
+    activities[i].addEventListener(
+      "mouseover",
+      function (event) {
+        // highlight the mouseover target
+        //   event.target.style.background = "orange";
+        event.target.style.cursor = "pointer";
+
+        //   // reset the color after a short delay
+        //   setTimeout(function () {
+        //     event.target.style.background = "green";
+        //   }, 500);
+      },
+      false
+    );
+  }
+}
