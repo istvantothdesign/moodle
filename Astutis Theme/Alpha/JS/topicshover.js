d69995617f03247360fccbcf8e17c948e7d35818
Istvan <istvan.toth@astutis.com>
2022-06-24 11:57:50 +0100

    Modifying explore more

--- Astutis Theme/Alpha/JS/topicshover.js
+++ Astutis Theme/Alpha/JS/topicshover.js
@@ -1,27 +1,32 @@
-const topics = document.querySelectorAll(".section-summary");
-console.log(topics);
+let astPageID = document.getElementById("page-course-view-topics");
 
-for (let i = 0; i < topics.length; ++i) {
-  topics[i].addEventListener("click", function () {
-    let link = this.childNodes[1].childNodes[1].childNodes[1].href;
-    console.log(link);
-    window.location.href = `${link}`;
-  });
+if (astPageID.id === "page-course-view-topics") {
+  console.log("yes");
+  const topics = document.querySelectorAll(".section-summary");
+  console.log(topics);
 
-  // This handler will be executed every time the cursor
-  // is moved over a different list item
-  topics[i].addEventListener(
-    "mouseover",
-    function (event) {
-      // highlight the mouseover target
-      //   event.target.style.background = "orange";
-      event.target.style.cursor = "pointer";
+  for (let i = 0; i < topics.length; ++i) {
+    topics[i].addEventListener("click", function () {
+      let link = this.childNodes[1].childNodes[1].childNodes[1].href;
+      console.log(link);
+      window.location.href = `${link}`;
+    });
 
-      //   // reset the color after a short delay
-      //   setTimeout(function () {
-      //     event.target.style.background = "green";
-      //   }, 500);
-    },
-    false
-  );
+    // This handler will be executed every time the cursor
+    // is moved over a different list item
+    topics[i].addEventListener(
+      "mouseover",
+      function (event) {
+        // highlight the mouseover target
+        //   event.target.style.background = "orange";
+        event.target.style.cursor = "pointer";
+
+        //   // reset the color after a short delay
+        //   setTimeout(function () {
+        //     event.target.style.background = "green";
+        //   }, 500);
+      },
+      false
+    );
+  }
 }
